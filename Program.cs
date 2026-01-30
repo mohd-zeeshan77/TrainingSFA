d28bb4fcb81d2a972dde56751d08d4d2ed61ea5c
Mohd Zeeshan khan <144587704+mohd-zeeshan77@users.noreply.github.com>
2026-01-30 14:14:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        /* Employee emp = new()
+         {
+             Id = 1,
+             FirstName = "John",
+             LastName = "Stew",
+             Salary = 25000.00
+         };
+         Employee emp2 = new()
+         {
+             Id = 2,
+             FirstName = "Cinna",
+             Salary = 1000.23
+         };
+         Employee emp3 = new()
+         {
+             Id = 3,
+             FirstName = "Jack      ",
+             LastName = "    Thyme     ",
+             Salary = 231000
+         };
+         emp.Display();
+         emp2.Display();
+         emp3.Display();*/
+
+
+        Calculator calc = new();
+        calc.add();
+        calc.sub();
+        calc.mul();
+
+        StudentDto? ob = null;
+        calc.save(ob);
+        //Console.ReadKey();
+    }
+}
